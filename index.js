491f028d4acec2ee48397a1a8f432f4148a51ac6
khoirularyan <khoirularyan123@gmail.com>
2025-10-02 06:51:42 +0700

    connect db + test api

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-import express from 'express'
-const app = express()
+import express from "express";
+import mongoose from "mongoose";
+const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+  console.log("Server is running on port 3000");
+});
 
-app.get('/', (req, res) => {
-    res.send('Hello, this is from Express.js! node')
-})
+app.get("/", (req, res) => {
+  res.send("Hello, this is from Express.jsadfs! ssdfsdffasdfsadfsdnode");
+});
+
+app.post("/api/products", (req, res) => {
+  res.send("Create a new product");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://khoirularyan:4zTxumyllyJ1Hf71@backenddb.4anlpub.mongodb.net/?retryWrites=true&w=majority&appName=BackendDb"
+  )
+  .then(() => console.log("Connected!"))
+  .catch((err) => console.error("Connection error:", err));
