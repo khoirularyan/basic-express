95305d82819e899c89365a9af3d8830d880483cc
khoirularyan <khoirularyan123@gmail.com>
2025-10-07 22:11:56 +0700

    delete product by id

--- index.js
+++ index.js
@@ -67,6 +67,21 @@ app.put("/api/product/:id", async (req, res) => {
   }
 });
 
+//delete produk by id
+app.delete("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json({ message: "delete success" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 //connect to database
 mongoose
   .connect(
