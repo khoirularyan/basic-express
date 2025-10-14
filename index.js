e460cadd4e29b8f635ee7d8d338086d23e403d06
khoirularyan <khoirularyan123@gmail.com>
2025-10-14 10:17:50 +0700

    test web hook

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import ProductRoutes from "./routes/product.route.js";
 
 dotenv.config();
 const app = express();
-
+//test web hook
 //middleware
 app.use(express.json());
 //untuk menerima data dari body parsing data
 app.use(express.urlencoded({ extended: true }));
+
+//routes
+app.use("/api/products", ProductRoutes);
+
+//routes atau endpoint
+app.get("/", (req, res) => {});
 
 const PORT = process.env.PORT || 3000;
 
